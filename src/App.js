01edc26db2d74cca119e8667a8ef4d89742ea224
parisa <mbkparisa@gmail.com>
2022-03-03 15:13:12 +0330

    make context and reducer

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-import { Fragment, useState } from 'react';
-import Cart from './components/Cart/Cart';
+import { useState } from 'react';
 
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
-  const [cartIsShow, setCartIsShow] = useState(false);
+  const [cartIsShown, setCartIsShown] = useState(false);
 
   const showCartHandler = () => {
-    setCartIsShow(true);
-   }
+    setCartIsShown(true);
+  };
 
-   const hideCartHandler =()=>{
-     setCartIsShow(false)
-   }
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
-    <Fragment>
-      {cartIsShow && <Cart onClose={hideCartHandler} />}
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
